99fdad6c17a7de3a75cd01b939b56125abbf8d84
CAPC02 <cesiapajaroc@hotmail.com>
2026-05-30 15:46:50 -0500

    Actividad de la semana 4

---
+++ Semana4/server.js
@@ -0,0 +1,1 @@
+﻿const express = require('express'); const app = express(); app.get('/', (req, res) => { res.send('Servidor funcionando correctamente'); }); app.listen(3000, () => { console.log('Servidor ejecutandose en puerto 3000'); });
